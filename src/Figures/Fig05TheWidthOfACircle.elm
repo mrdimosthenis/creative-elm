ac3a0076dddb4c300a1c9f751253c34f090cdf5a
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-03-30 17:00:50 +0200

    figure 5

---
+++ src/Figures/Fig05TheWidthOfACircle.elm
@@ -0,0 +1,29 @@
+module Figures.Fig05TheWidthOfACircle exposing (..)
+
+
+import Html exposing (Html)
+import Collage
+import Collage.Render as Render
+import Collage.Layout as Layout
+
+
+main : Html msg
+main =
+    let
+        smallCircle =
+            Collage.circle 20
+                |> Collage.outlined Collage.defaultLineStyle
+        largeCircle =
+            Collage.circle 60
+                |> Collage.outlined Collage.defaultLineStyle
+    in
+    [ smallCircle
+    , smallCircle
+    , smallCircle ]
+        |> Layout.horizontal
+        |> Layout.align Layout.base
+        |> List.singleton
+        |> List.append [ largeCircle ]
+        |> Layout.stack
+        |> Render.svg
+
