1fd54ae79b534c43b8b95f2bbb865ffe0097a7a4
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-03-31 21:50:26 +0300

    figure 19

---
+++ src/Figures/Fig19.elm
@@ -0,0 +1,64 @@
+module Figures.Fig19 exposing (..)
+
+
+import Html exposing (Html)
+import Collage
+import Collage.Render as Render
+import Collage.Layout as Layout
+import Color
+
+
+main : Html msg
+main =
+    let
+        roof =
+            Collage.triangle 50
+                |> Collage.filled (Collage.uniform Color.brown)
+        frontDoor =
+            Collage.rectangle 50 25
+                |> Collage.filled (Collage.uniform Color.red)
+                |> List.singleton
+                |> List.append [ Collage.filled (Collage.uniform Color.black) (Collage.rectangle 10 25) ]
+                |> Layout.stack
+                |> List.singleton
+                |> List.append [ Collage.filled (Collage.uniform Color.red) (Collage.rectangle 50 15) ]
+                |> Layout.vertical
+        house =
+            [ roof, frontDoor ]
+            |> Layout.vertical
+            |> Layout.align Layout.bottom
+        tree =
+            Collage.rectangle 10 20
+                |> Collage.filled (Collage.uniform Color.brown)
+                |> List.singleton
+                |> List.append [ Collage.filled (Collage.uniform Color.green) (Collage.circle 25) ]
+                |> Layout.vertical
+                |> Layout.align Layout.bottom
+        streetSegment =
+            Collage.rectangle 15 3
+                |> Collage.filled (Collage.uniform Color.black)
+                |> List.singleton
+                |> List.append [ Collage.filled (Collage.uniform Color.yellow) (Collage.rectangle 30 3) ]
+                |> Layout.horizontal
+                |> Layout.align Layout.base
+                |> List.singleton
+                |> List.append [ Collage.filled (Collage.uniform Color.black) (Collage.rectangle 45 6) ]
+                |> List.reverse
+                |> Layout.vertical
+        street =
+            List.repeat 3 streetSegment
+                |> Layout.horizontal
+                |> Layout.align Layout.base
+        houseAndGarden =
+            [ house, tree ]
+                |> Layout.horizontal
+                |> Layout.align Layout.base
+                |> List.singleton
+                |> List.append [ street ]
+                |> List.reverse
+                |> Layout.vertical
+    in
+    List.repeat 3 houseAndGarden
+        |> Layout.horizontal
+        |> Render.svg
+
