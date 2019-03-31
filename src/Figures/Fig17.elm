1dc6d589bd3bb1e5df653c53889829f902488e95
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-03-31 21:18:53 +0300

    figure 17

---
+++ src/Figures/Fig17.elm
@@ -0,0 +1,35 @@
+module Figures.Fig17 exposing (..)
+
+
+import Html exposing (Html)
+import Collage exposing (defaultLineStyle)
+import Collage.Render as Render
+import Collage.Layout as Layout
+import Color
+
+
+spin : Float -> Color.Color -> Color.Color
+spin angle color =
+    let
+        { hue, saturation, lightness, alpha } =
+            Color.toHsla color
+    in
+    Color.hsla (hue + angle) saturation lightness alpha
+
+
+main : Html msg
+main =
+    let
+        box =
+            Collage.rectangle 40 40
+                |> Collage.styled
+                    ( Color.lightBlue
+                        |> spin (degrees 30)
+                        |> Collage.uniform
+                    , { defaultLineStyle | fill = Collage.uniform Color.darkBlue }
+                    )
+    in
+    List.repeat 5 box
+        |> Layout.horizontal
+        |> Render.svg
+
