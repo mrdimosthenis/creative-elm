e3e156ce935c88560bed8a48a75c94e4a65061de
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-04-06 15:54:55 +0300

    figure 23

---
+++ src/Figures/Fig23.elm
@@ -0,0 +1,35 @@
+module Figures.Fig23 exposing (..)
+
+
+import Html exposing (Html)
+import Collage
+import Collage.Render as Render
+import Collage.Layout as Layout
+
+
+cross : Int -> Collage.Collage msg
+cross n =
+    let
+        unit = Collage.outlined Collage.defaultLineStyle (Collage.circle 20)
+    in
+    case n of
+        0 -> unit
+        _ -> let
+                inBetween = [ unit, cross (n - 1),  unit ]
+                                |> Layout.vertical
+                                |> Layout.align Layout.base
+             in
+             [ unit, inBetween, unit ]
+                |> Layout.horizontal
+                |> Layout.align Layout.base
+
+
+main : Html msg
+main =
+    List.range 0 3
+        |> List.map cross
+        |> List.intersperse (Layout.spacer 20 0)
+        |> Layout.horizontal
+        |> Layout.align Layout.base
+        |> Render.svg
+
