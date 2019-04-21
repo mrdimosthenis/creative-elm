563a59391eacf3979e44e699167338bdefecea26
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-04-21 14:57:31 +0300

    figure 35

--- src/Figures/Fig35.elm
+++ src/Figures/Fig35.elm
@@ -1,4 +1,4 @@
-module Figures.Fig34 exposing (..)
+module Figures.Fig35 exposing (..)
 
 
 import Html exposing (Html)
@@ -7,33 +7,30 @@ import Svg
 import Svg.Attributes as Attr
 
 
-triangleAt : Vector2d.Vector2d -> Svg.Svg msg
-triangleAt point =
+dotAt : Vector2d.Vector2d -> Svg.Svg msg
+dotAt point =
     let
         x =
             point |> Vector2d.xComponent |> truncate |> String.fromInt
         y =
             point |> Vector2d.yComponent |> truncate |> String.fromInt
     in
-    Svg.svg
-        [ Attr.x x
-        , Attr.y y
+    Svg.circle
+        [ Attr.cx x
+        , Attr.cy y
+        , Attr.r "1.5"
+        , Attr.fill "blue"
         ]
-        [ Svg.polyline
-            [ Attr.fill "none"
-            , Attr.stroke "black"
-            , Attr.points "5,0 0,10 10,10 5,0"
-            ]
-            []
-        ]
+        []
 
 
-dotAtParamCircle : Float -> Svg.Svg msg
-dotAtParamCircle angle =
-    (200, angle)
+rose : Float -> Svg.Svg msg
+rose angle =
+    (200 * cos (angle * 7)
+    , angle)
         |> Vector2d.fromPolarComponents
         |> Vector2d.sum (Vector2d.fromComponents (200, 200))
-        |> triangleAt
+        |> dotAt
 
 
 loop : Float -> Int -> Svg.Svg msg
@@ -44,7 +41,7 @@ loop step count =
     in
     case count of
         0 -> Svg.g [] []
-        _ -> Svg.g [] [ dotAtParamCircle angle, loop step (count - 1) ]
+        _ -> Svg.g [] [ rose angle, loop step (count - 1) ]
 
 
 sample : Int -> Svg.Svg msg
@@ -62,5 +59,5 @@ main =
         [ Attr.width "410"
         , Attr.height "410"
         ]
-        [ sample 72 ]
+        [ sample 300 ]
 
