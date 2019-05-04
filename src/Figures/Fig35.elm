8a3aa5da6266218d6926cdc5eb7a78e07f0bf3b9
mrdimosthenis <mrdimosthenis@hotmail.com>
2019-05-04 17:22:52 +0300

    typed fig35

--- src/Figures/Fig35.elm
+++ src/Figures/Fig35.elm
@@ -3,23 +3,27 @@ module Figures.Fig35 exposing (..)
 
 import Html exposing (Html)
 import Vector2d
-import Svg
-import Svg.Attributes as Attr
+import TypedSvg
+import TypedSvg.Core as Svg
+import TypedSvg.Attributes as Attr
+import TypedSvg.Attributes.InPx as PxAttr
+import TypedSvg.Types as SvgTypes
+import Color
 
 
 dotAt : Vector2d.Vector2d -> Svg.Svg msg
 dotAt point =
     let
         x =
-            point |> Vector2d.xComponent |> truncate |> String.fromInt
+            Vector2d.xComponent point
         y =
-            point |> Vector2d.yComponent |> truncate |> String.fromInt
+            Vector2d.yComponent point
     in
-    Svg.circle
-        [ Attr.cx x
-        , Attr.cy y
-        , Attr.r "1.5"
-        , Attr.fill "blue"
+    TypedSvg.circle
+        [ PxAttr.cx x
+        , PxAttr.cy y
+        , PxAttr.r 1.5
+        , Attr.fill (SvgTypes.Fill Color.blue)
         ]
         []
 
@@ -40,8 +44,8 @@ loop step count =
             step * toFloat count
     in
     case count of
-        0 -> Svg.g [] []
-        _ -> Svg.g [] [ rose angle, loop step (count - 1) ]
+        0 -> TypedSvg.g [] []
+        _ -> TypedSvg.g [] [ rose angle, loop step (count - 1) ]
 
 
 sample : Int -> Svg.Svg msg
@@ -55,9 +59,9 @@ sample n =
 
 main : Html msg
 main =
-    Svg.svg
-        [ Attr.width "410"
-        , Attr.height "410"
+    TypedSvg.svg
+        [ PxAttr.width 410.0
+        , PxAttr.height 410.0
         ]
         [ sample 300 ]
 
